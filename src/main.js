3ffab49c5a8bab8b15357f54ad0fe4b6a5944b73
Eduardo <you@example.com>
2019-12-08 14:08:24 -0300

    Adjust Main.js - Using Enums

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 const Product = require('./product');
 const CarInsurance = require('./carInsurance');
+const enumProducts = require('./enums');
 
 let listOfProducts = [
-    new Product('Medium Coverage', 10, 20),
-    new Product('Full Coverage', 2, 0),
-    new Product('Low Coverage', 5, 7),
-    new Product('Mega Coverage', 0, 80),
-    new Product('Mega Coverage', -1, 80),
-    new Product('Special Full Coverage', 15, 20),
-    new Product('Special Full Coverage', 10, 49),
-    new Product('Special Full Coverage', 5, 49),
-    new Product('Super Sale', 3, 6),
+    new Product(enumProducts.MEDIUM, 10, 20),
+    new Product(enumProducts.FULL, 2, 0),
+    new Product(enumProducts.LOW, 5, 7),
+    new Product(enumProducts.MEGA, 0, 80),
+    new Product(enumProducts.MEGA, -1, 80),
+    new Product(enumProducts.SPECIALFULL, 15, 20),
+    new Product(enumProducts.SPECIALFULL, 10, 49),
+    new Product(enumProducts.SPECIALFULL, 5, 49),
+    new Product(enumProducts.SUPERSALE, 3, 6),
 ];
 
 const carInsurance = new CarInsurance(listOfProducts);
